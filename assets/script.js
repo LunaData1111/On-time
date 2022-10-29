c2fb54d113629a53e5cc4f3b8f6a799ac140af2c
Christian Luna <christianlunaduenas@gmail.com>
2022-10-28 17:32:59 -0700

    finishing up

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
-
-$("#hour-7 .description").val(localStorage.getItem("hour-9"));
-$("#hour-8 .description").val(localStorage.getItem("hour-10"));
-$("#hour-9 .description").val(localStorage.getItem("hour-11"));
-$("#hour-10 .description").val(localStorage.getItem("hour-12"));
-$("#hour-11 .description").val(localStorage.getItem("hour-13"));
-$("#hour-12 .description").val(localStorage.getItem("hour-14"));
-$("#hour-13 .description").val(localStorage.getItem("hour-15"));
-$("#hour-14 .description").val(localStorage.getItem("hour-16"));
-$("#hour-15 .description").val(localStorage.getItem("hour-17"));
-
+// These are the time frames that the client may use to arrange their schedule.
+$("#hour-7 .description").val(localStorage.getItem("hour-7"));
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+// Here we have the save button and saving to local storage 
 $(".saveBtn").on("click", function () {
   var textvalue = $(this).siblings(".description").val();
   var hour = $(this).parent().attr("id");
